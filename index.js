637d9f30bbe3380ebbff91c2c7979c13eefa831a
AmineLeCepe <lionannaba@gmail.com>
2024-09-10 19:23:04 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('combined'));
+app.use(express.static('public'));
+
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+
+app.listen(port, () => {
+    console.log("Server started on port " + port);
+});
