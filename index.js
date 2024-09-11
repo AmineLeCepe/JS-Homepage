587b4cd0aa0205de445f981cafd370b1bbaa61e1
AmineLeCepe <lionannaba@gmail.com>
2024-09-11 16:43:31 +0100

    Add login page CSS

--- index.js
+++ index.js
@@ -5,20 +5,33 @@ import morgan from "morgan";
 const app = express();
 const port = process.env.PORT || 3000;
 
+let currentUser = "";
+
+// Middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('combined'));
 app.use(express.static('public'));
 
-
+// Loads the homepage
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {
+        user: currentUser,
+    });
 });
 
+// Loads the login page
 app.get("/login", (req, res) => {
     res.render("login.ejs");
-})
+});
 
 
+// Intercepts POST requests for user login,
+// Request body: { username: "", password: "" }
+app.post("/login", (req, res) => {
+    console.log(req.body);
+});
+
+// Server start
 app.listen(port, () => {
     console.log("Server started on port " + port);
 });
