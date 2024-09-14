011e3e7cf7228d514a67be28195fe2ef44f28dc4
AmineLeCepe <lionannaba@gmail.com>
2024-09-14 22:50:20 +0100

    Implement bcrypt for login and registration process

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import bodyParser from "body-parser";
 import morgan from "morgan";
 import pg from "pg";
 import env from "dotenv";
-
-
-const app = express();
-const port = process.env.PORT || 3000;
+import bcrypt from "bcrypt";
+import e from "express";
 
 env.config();
+
+const app = express();
+const port = process.env.PORT;
+const saltRounds = 10;
 
 // Setting up connection with database
 const db = new pg.Client({
@@ -39,6 +41,12 @@ app.get("/login", (req, res) => {
     res.render("login.ejs");
 });
 
+// Loads the register page
+
+app.get("/register", (req, res) => {
+    res.render("register.ejs");
+});
+
 
 // Intercepts POST requests for user login,
 // Request body: { username: "", password: "" }
@@ -52,16 +60,24 @@ app.post("/login", async (req, res) => {
             [submittedUser]
         );
         if (result.rows.length > 0) {
-            if (result.rows[0].password == submittedPassword) {
-                res.render("index.ejs", {
-                    currentUser: result.rows[0].username,
-                });
-            } else {
-                res.render("login.ejs", {
-                    errorMessage: "Login failed: passwords do not match"
-                });
-                console.log("Login failed: passwords do not match");
-            }
+            const dbHashedPassword = result.rows[0].password;
+            bcrypt.compare(submittedPassword, dbHashedPassword, (err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    if (result) {
+                        console.log("Login successful");
+                        res.render("index.ejs", {
+                            currentUser: submittedUser
+                        });
+                    } else {
+                        console.log("Login failed: passwords do not match")
+                        res.render("login.ejs", {
+                            errorMessage: "Login failed: passwords do not match",
+                        });
+                    }
+                }
+            });
         } else {
             res.render("login.ejs", {
                 errorMessage: "Login failed: user not found"
@@ -73,6 +89,47 @@ app.post("/login", async (req, res) => {
     };
 
 });
+
+// Intercepts POST request for user registration,
+// Request body: { username: "", password: "", passwordConfirm: "" }
+
+app.post("/register", async (req, res) => {
+    try {
+        const result = await db.query(
+            "SELECT * FROM users WHERE username = $1",
+            [req.body.username]
+        );
+        if (result.rows.length > 0) {
+            res.render("register.ejs", {
+                errorMessage: "Register failed: user already exists"
+            });
+            console.log("Register failed: user alraedy exists");
+        } else {
+            if (req.body.password === req.body.passwordConfirm) {
+                bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
+                    if (err) {
+                        console.error(err);
+                    }
+                    const result = await db.query(
+                        "INSERT INTO users (username, password) VALUES ($1, $2)",
+                        [req.body.username, hash]
+                    );
+                    console.log(`Result: ${result}`);
+                    console.log("Registration complete");
+                    res.redirect("/");
+                });
+            } else {
+                res.render("register.ejs", {
+                    errorMessage: "Register failed: passwords do not match"
+                });
+                console.log("Register failed: passwords do not match");
+            }
+        }
+    } catch (err) {
+        console.error(err);
+        res.redirect("/register");
+    }
+})
 
 // Server start
 app.listen(port, () => {
